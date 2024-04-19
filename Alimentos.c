d137560d590bf7152ea8ae05b1e75fa3d7ac0ac8
gabriel-castro <gabrielcastro.developer@gmail.com>
2024-04-19 16:15:03 -0300

    Alimentos fixed

--- Alimentos.c
+++ Alimentos.c
@@ -7,11 +7,11 @@ static char nomeProdutoAlimentos[9][50] =
     {"Feijao"},
     {"Acucar (1kg)"},
     {"Sal"},
-    {"Macarrao"},
+    {"Farinha de Trigo (1kg)"},
     {"Azeite"},
     {"Farinha (1kg)"}
 };
-static float precoAlimentos[9] = {19.99, 5.99, 4.5, 8.00, 5.00, 2.50, 3.00, 15.00, 3.50};
+static float precoAlimentos[9] = {19.99, 5.90, 4.5, 8.00, 5.00, 2.00, 5.00, 15.00, 3.50};
 static int repeticaoAlimentos = 9;
 
 //Funcao que recebe o codigo, reenvia para a funcao de pegar a quantidade, e retorna o preco correspodente
@@ -20,46 +20,46 @@ static float retornoPrecoAlimentos(int codigoCompra)
     float preco = 0;
     int quantidade = 0;
 
-    switch(codigoCompra)
-    {
-    case 21:
-        preco = precoAlimentos[0];
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[0], precoAlimentos[0], codigosAlimentos[0]);
-        break;
-    case 22:
-        preco = precoAlimentos[1];
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[1], precoAlimentos[1], codigosAlimentos[1]);
-        break;
-    case 23:
-        preco = precoAlimentos[2];
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[2], precoAlimentos[2], codigosAlimentos[2]);
-        break;
-    case 24:
-        preco = precoAlimentos[3];
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[3], precoAlimentos[3], codigosAlimentos[3]);
-        break;
-    case 25:
-        preco = precoAlimentos[4];
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
-        break;
-    case 26:
-        preco = precoAlimentos[5]; 
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
-        break;
-     case 27:
-        preco = precoAlimentos[6]; 
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
-        break;
-     case 28:
-        preco = precoAlimentos[7]; 
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
-     case 29:
-        preco = precoAlimentos[8]; 
-        quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
-        break;
-         default:
-        preco = 0.0;
-        break;
+    switch(codigoCompra){
+        case 21:
+            preco = precoAlimentos[0];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[0], precoAlimentos[0], codigosAlimentos[0]);
+            break;
+        case 22:
+            preco = precoAlimentos[1];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[1], precoAlimentos[1], codigosAlimentos[1]);
+            break;
+        case 23:
+            preco = precoAlimentos[2];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[2], precoAlimentos[2], codigosAlimentos[2]);
+            break;
+        case 24:
+            preco = precoAlimentos[3];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[3], precoAlimentos[3], codigosAlimentos[3]);
+            break;
+        case 25:
+            preco = precoAlimentos[4];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[4], precoAlimentos[4], codigosAlimentos[4]);
+            break;
+        case 26:
+            preco = precoAlimentos[5];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[5], precoAlimentos[5], codigosAlimentos[5]);
+            break;
+        case 27:
+            preco = precoAlimentos[6];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[6], precoAlimentos[6], codigosAlimentos[6]);
+            break;
+        case 28:
+            preco = precoAlimentos[7];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[7], precoAlimentos[7], codigosAlimentos[7]);
+            break;
+        case 29:
+            preco = precoAlimentos[8];
+            quantidade = pegarQuantidade(nomeProdutoAlimentos[8], precoAlimentos[8], codigosAlimentos[8]);
+            break;
+        default:
+            preco = 0.0;
+            break;
     }
 
     preco = (float) preco * quantidade;
