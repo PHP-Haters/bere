98b8d9ad63fafb1db4b911d7efb7e3d58d3387f7
foster <joaopfr06@gmail.com>
2024-05-22 21:03:05 -0300

    menu inicial

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "menu.c"
+
+void main(){
+    
+    mainMenu();
+
+}
---
+++ menu.c
@@ -0,0 +1,14 @@
+
+void mainMenu(){
+
+    printf(".________- MERCEARIA DA BERE -_________.\n");
+    printf("|                                      |\n");
+    printf("| 1 > Cadastro                         |\n");
+    printf("| 2 > Vendas                           |\n");
+    printf("| 3 > Abertura de Caixa                |\n");
+    printf("| 4 > Fechamento do Caixa              |\n");
+    printf("| 5 > Relatorios                       |\n");
+    printf("| 6 > Sair                             |\n");
+    printf("|______________________________________|");
+    
+}
