eee6a78820d0e234be885de93925b64f30c92437
foster <joaopfr06@gmail.com>
2024-05-22 21:11:59 -0300

    escolhas preparada

--- main.c
+++ main.c
@@ -2,7 +2,44 @@
 #include "menu.c"
 
 void main(){
-    
-    mainMenu();
+    inicializeShop();
+}
 
+void inicializeShop() {
+    mainMenu();
+    redirectUser();
+
+}
+
+
+void redirectUser(){
+
+    int choice = 0;
+
+    scanf("%d", &choice);
+    
+    switch (choice)
+    {
+    case 1:
+        //adicionar cadastro
+        break;
+    case 2:
+        //adicionar vendas
+        break;
+    case 3:
+        //adicionar abertura de caixa
+        break;
+    case 4:
+        //adicionar fechamento de caixas
+        break;
+    case 5:
+        //adicionar relatorios 
+        break;
+    case 6:
+        //adicionar sair
+        break;
+    default:
+        inicializeShop();
+        break;
+    }
 }
